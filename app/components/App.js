921f3a43c1894897f1c3619b7c1429aa6de0a590
Patrick Morgan <patrick.morgan@live.com.au>
2017-05-21 18:39:47 +1000

    React 'hello world' app created

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <h1>Hello World!</h1>
+                <p>Sick as bro</p>
+            </div>
+        )
+    }
+}
+
+module.exports = App;
